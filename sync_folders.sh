20856e51e7f1c3a752cf41d18c88fe5357164025
Juanma <115048042+MoonPyxx@users.noreply.github.com>
2023-12-09 13:38:03 +0100

    Update sync_folders.sh

--- sync_folders.sh
+++ sync_folders.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 path_to_folder_a path_to_folder_b path_to_log_file"
+    exit 1
+fi
 # folder paths
-folder_a="/path/to/folder/a"
-folder_b="/path/to/folder/b"
-
-# log path
-log_file="/path/to/logfile.txt"
+folder_a="$1"
+folder_b="$2"
+log_file="$3"
 
 # Syncing from folder A to folder B (not backwards)
 rsync -av --update --itemize-changes "$folder_a/" "$folder_b/" | tee -a "$log_file"
